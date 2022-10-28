51666f07e4006c354459c1682f5a0ade1be648b5
Xavier Perez <xap5xap@users.noreply.github.com>
2022-10-28 17:28:57 -0500

    fix

--- src/components/DevelopmentProcessSection.tsx
+++ src/components/DevelopmentProcessSection.tsx
@@ -1,11 +1,10 @@
 import { Box, Container } from "@mui/material";
-import { greyDark } from "../theme/brandingTheme";
 import DevelopmentProcessImage from "./DevelopmentProcessImage";
 import SectionTitle from "./SectionTitle";
 
 const DevelopmentProcessSection = () => {
   return (
-    <Box component="section" sx={{ backgroundColor: greyDark[400], py: 5 }}>
+    <Box component="section" sx={{ py: 5 }}>
       <Container>
         <SectionTitle
           title="What you get"
