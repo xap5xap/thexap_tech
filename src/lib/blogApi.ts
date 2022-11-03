94811039d48a71fa35f7c2a87d792196112e814f
Xavier Perez <xap5xap@users.noreply.github.com>
2022-11-02 19:18:28 -0500

    add blog api

---
+++ src/lib/blogApi.ts
@@ -0,0 +1,38 @@
+const fetchGraphQL = async (query: any, preview: boolean = false) => {
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          preview
+            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+            : process.env.CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+      body: JSON.stringify({ query }),
+    }
+  ).then((response) => response.json());
+};
+
+export const getAllPostForBlogPage = async () => {
+  const entry = await fetchGraphQL(`query {
+        blogCollection(order:date_DESC){
+          items{
+            title
+            thumbnailImage{
+              url
+            }
+            excerpt
+            contentfulMetadata{
+              tags{
+                 id
+                name
+              }
+            }
+          }
+        }
+      }`);
+  return entry?.data?.blogCollection?.items;
+};
